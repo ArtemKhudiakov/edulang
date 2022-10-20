66fc728f57c1be5b6e8ce7510ea5742eee261fa2
ArtemKhudiakov <artemka86ber@gmail.com>
2022-10-20 12:18:15 -0300

    feat: types

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,15 @@
-let a: number = 1;
-let b: string = '1';
-let c = a + b;
+// let a: number = 1;
+// let b: string = '1';
+// let c = a + b;
+//
 
+type TUser = {
+    name: string
+    email: string
+    password: number
+    language: string
+}
 
+function greet(user: TUser) {
+    console.log(user.email)
+}
